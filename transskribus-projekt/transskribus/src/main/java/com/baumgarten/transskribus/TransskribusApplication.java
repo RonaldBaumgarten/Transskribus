4a05caa39d5ae2aa0f92308f10116037f6ea02ec
RonaldBaumgarten <baumgartendj@gmail.com>
2026-04-17 18:45:40 +0200

    create project structure

---
+++ transskribus-projekt/transskribus/src/main/java/com/baumgarten/transskribus/TransskribusApplication.java
@@ -0,0 +1,13 @@
+package com.baumgarten.transskribus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransskribusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransskribusApplication.class, args);
+	}
+
+}
---
+++ transskribus-projekt/transskribus/src/test/java/com/baumgarten/transskribus/TransskribusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baumgarten.transskribus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransskribusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
